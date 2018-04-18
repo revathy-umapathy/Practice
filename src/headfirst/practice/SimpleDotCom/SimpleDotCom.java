746015cc3b9f159945ae5416f5e409af7916c68f
Revathy <rey14510@gmail.com>
2018-04-18 17:42:32 +0530

    Checkin 1 / SimpleDotCom

--- src/headfirst/practice/SimpleDotCom/SimpleDotCom.java
+++ src/headfirst/practice/SimpleDotCom/SimpleDotCom.java
@@ -2,13 +2,26 @@ package headfirst.practice.SimpleDotCom;
 
 public class SimpleDotCom {
 int[] locationCells;
-int numOfHit=0;
+int numOfHits=0;
 
 public void setLocationCells(int[] locationCells) {
 	this.locationCells = locationCells;
 }
 
 public String checkYourself(String stringGuess){
-	return stringGuess;
+	int guess=Integer.parseInt(stringGuess);
+	String result = "miss";
+	for(int cell: locationCells){
+		if(guess == cell){
+				result ="hit";
+				numOfHits++;
+		break;
+	}
+}	
+	if(numOfHits == locationCells.length){
+		result="kill";
+	}
+System.out.println(result);
+return result;
 }
 }
